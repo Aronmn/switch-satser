a3ec392b52261fe32d759cfc33d2f5cec49e95ce
Aron <aronmn@vgy.se>
2016-10-22 18:59:53 +0200

    Övning 1-2

--- Switch satser/Program.cs
+++ Switch satser/Program.cs
@@ -12,11 +12,9 @@ namespace Switch_satser
         {
             //övning 1
 
-            Console.WriteLine("Skriv in ett hel tal");
-
+            Console.WriteLine("Skriv in ett heltal");
             int tal = int.Parse(Console.ReadLine());
-            
-
+           
             switch (tal)
             {
                 case 1:
@@ -33,6 +31,35 @@ namespace Switch_satser
                     break;
                 default:
                     Console.WriteLine("Ogilitigt alternativ");
+                    break;
+            }
+
+            //Övning 2
+
+            Console.WriteLine("Ange ett vädersträck: N,S,E,W");
+            string streck = Console.ReadLine();
+            char x = char.Parse(streck);
+
+            switch (x)
+            {
+                case 'N':
+                case 'n':
+                    Console.WriteLine("You walk north");
+                    break;
+                case 'S':
+                case 's':
+                    Console.WriteLine("You travel south");
+                    break;
+                case 'E':
+                case 'e':
+                    Console.WriteLine("You wander east");
+                    break;
+                case 'W':
+                case 'w':
+                    Console.WriteLine("You stroll west");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand!");
                     break;
             }
         }
